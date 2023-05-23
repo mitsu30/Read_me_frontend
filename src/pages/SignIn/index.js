deffc485e1b1c786fca93259cc8fec4ca130506d
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-23 15:30:23 +0900

    追加 React-shareインストール、お試しシェアボタン実装

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -7,11 +6,11 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Image from "next/image";
-import EditIcon from '@mui/icons-material/Edit';
+import { TwitterShareButton, TwitterIcon} from "react-share";
+
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
@@ -34,102 +33,111 @@ export default function SignInSide() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }} justifyContent="center">
-        <CssBaseline />
-          <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
-            <Box
-              sx={{
-                my: 20,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Typography component="h1" variant="h5">
-                入力してね♪
-              </Typography>
-              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                <TextField
-                  color="secondary"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="nickname"
-                  label="ニックネームは？"
-                  name="nickname"
-                  autoFocus
-                />
-                <TextField
-                  color="secondary"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="hobby"
-                  label="しゅみは？"
-                  id="hobby"
-                />
-                <TextField
-                  color="secondary"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="favoriteCT"
-                  label="推しCTは？"
-                  id="favoriteCT"
-                />
-                <Container
-                  maxWidth="sm"
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Button 
-                    type="submit"
-                    variant="contained"
-                    sx={{ 
-                      mt: 3, 
-                      mb: 2, 
-                      width: '50%',
-                      backgroundColor: '#FF82B2',
-                      color: '#000000'  }}
+    <>
+      <ThemeProvider theme={theme}>
+        <Grid container component="main" sx={{ height: '100vh' }} justifyContent="center">
+          <CssBaseline />
+            <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+              <Box
+                sx={{
+                  my: 20,
+                  mx: 4,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography component="h1" variant="h5">
+                  入力してね♪
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                  <TextField
+                    color="secondary"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="nickname"
+                    label="ニックネームは？"
+                    name="nickname"
+                    autoFocus
+                  />
+                  <TextField
+                    color="secondary"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="hobby"
+                    label="しゅみは？"
+                    id="hobby"
+                  />
+                  <TextField
+                    color="secondary"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="favoriteCT"
+                    label="推しCTは？"
+                    id="favoriteCT"
+                  />
+                  <Container
+                    maxWidth="sm"
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
                   >
-                    これでつくる!
-                  </Button>
-                </Container>
+                    <Button 
+                      type="submit"
+                      variant="contained"
+                      sx={{ 
+                        mt: 3, 
+                        mb: 2, 
+                        width: '50%',
+                        backgroundColor: '#FF82B2',
+                        color: '#000000'  }}
+                    >
+                      これでつくる!
+                    </Button>
+                  </Container>
+                </Box>
               </Box>
-            </Box>
+            </Grid>
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={6}
+            sx={{
+              my:20,
+              backgroundImage: "/public/template1.png",
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              
+            }}
+          >
+            <Container
+                maxWidth="sm"
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <Typography component="h1" variant="h5">
+                  プロフィール帳のデザインはこれだよ！
+                </Typography>
+              </Container>
+          <Image src="/template1.png" height='630px' width='1200px'/>
           </Grid>
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={6}
-          sx={{
-            my:20,
-            backgroundImage: "/public/template1.png",
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            
-          }}
-        >
-          <Container
-              maxWidth="sm"
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}
-            >
-              <Typography component="h1" variant="h5">
-                プロフィール帳のデザインはこれだよ！
-              </Typography>
-            </Container>
-        <Image src="/template1.png" height='630px' width='1200px'/>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
+
+      <TwitterShareButton 
+        url="https://prism-cube.com" 
+        title="りーどみー"
+        >
+        <TwitterIcon size={30} round={true} />
+      </TwitterShareButton>
+  </>
   );
 }
