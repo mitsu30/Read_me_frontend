c9d625288c3e7d3cdd38312ce5c510d55b2adc04
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-26 15:25:14 +0900

    追加 next-seo

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,30 +1,46 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import { TwitterShareButton, TwitterIcon} from "react-share";
+import Seo from '../../components/seo'
 
 export default function ResultPage() {
   const router = useRouter();
   const { imageUrl } = router.query;
-  const [currentURL, setCurrentURL] = useState('');
-
-  useEffect(() => {
-    setCurrentURL(window.location.href);
-  }, []);
 
   // router.isReady が true になるまで待つ
   if (!router.isReady) return null;
 
   return (
-    <div>
-      <img src={imageUrl} alt="Generated" />
-      <TwitterShareButton
-        url={currentURL}
-        title="Check out this image!"
-        hashtags={["ImageGenerator"]}
-        related={["TwitterHandle"]}
-      >
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
-    </div>
+    <>
+        <Seo
+        pageTitle={'site title'}
+        pageDescription={'site description'}
+        pageImg={'https://demo.com'}
+        pageImgWidth={1280}
+        pageImgHeight={960}
+      />
+      <h3>サンプル</h3>
+
+      <div>
+        <img src={imageUrl} alt="Generated" />
+        <TwitterShareButton
+          url={currentURL}
+          title="Check out this image!"
+          hashtags={["ImageGenerator"]}
+          related={["TwitterHandle"]}
+        >
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+      </div>
+    </>
   );
 }
+
+
+
+
+
+
+export async function getStaticProps() {
+  
+}
+
