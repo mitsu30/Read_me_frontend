6701f8b729b281ee682f36d890aadfbb4217dcd5
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-29 14:25:59 +0900

    修正 ワーニング対応 グローバルのzインデックス

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,22 +15,21 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
-        <Image
-          src="/background.png"
-          alt="Background image"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          zIndex={-1}
-        />
-        <div style={{position: 'relative', zIndex: 0}}>
-          <Header />
-          <Component {...pageProps} />
-          <Footer />
+        <div style={{ position: 'relative', height: '100vh', width: '100vw', zIndex: -1 }}>
+          <Image
+            src="/background.png"
+            alt="Background image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+          <div style={{position: 'relative', zIndex: 0}}>
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+          </div>
         </div>
-      </div>
-    </ThemeProvider>
+    </ThemeProvider> 
   );
 }
 
