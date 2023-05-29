df6a39615df1513b252b9a02cd43b80ff2e135e3
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-29 15:20:27 +0900

    追加 トップページにボタンを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,13 @@
+import Button from '@mui/material/Button';
+import { useRouter } from 'next/router';
+
 export default function HomePage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/create'); // Assuming your create page route is '/create'
+  };
+
   return (
     <div style={{
       position: 'fixed',
@@ -23,9 +32,20 @@ export default function HomePage() {
       {/* Here is the rest of your content */}
       <div style={{
         position: 'relative',
-        zIndex: 100
+        zIndex: 100,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh' 
       }}>
         {/* Rest of your content goes here */}
+        <Button 
+          variant="contained"
+          style={{ backgroundColor: '#FF82B2', color: '#000000', position: 'absolute', bottom: '10%' }}
+          onClick={handleClick}
+        >
+          はじめる
+        </Button>
       </div>
     </div>
   );
