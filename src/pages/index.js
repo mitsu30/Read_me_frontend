0d7a410f8bbb9945e4659407ac6b911fb6c66fae
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-29 14:22:23 +0900

    追加 トップページアニメーション

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,32 @@
-export default function App () {
+export default function HomePage() {
   return (
-    <>
-      <h3>トップページだよ</h3>
-    </>
+    <div style={{
+      position: 'fixed',
+      right: 0,
+      bottom: 0,
+      width: '100%',  // change from minWidth
+      height: '100%', // change from minHeight
+      zIndex: -100
+    }}>
+      <video autoPlay loop muted style={{
+        position: 'fixed',
+        right: '50%',
+        bottom: '50%',
+        width: '100%',  // change from minWidth
+        height: '100%', // change from minHeight
+        transform: 'translate(50%, 50%)',
+        objectFit: 'cover'
+      }}>
+        <source src="/video/top.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      {/* Here is the rest of your content */}
+      <div style={{
+        position: 'relative',
+        zIndex: 100
+      }}>
+        {/* Rest of your content goes here */}
+      </div>
+    </div>
   );
-};
+}
