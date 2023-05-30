42cb1f3fadbb712f89ad4a2662f00bd98af1e5ae
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-30 13:57:27 +0900

    追加 トップページレイアウト変更2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   const router = useRouter();
 
   const handleClick = () => {
-    router.push('/create'); // Assuming your create page route is '/create'
+    router.push('/create'); 
   };
 
   return (
