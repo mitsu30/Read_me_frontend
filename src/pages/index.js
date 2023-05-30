4b4ff63537968243b7badfb83f5cbbb9dc73da5a
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-30 13:07:31 +0900

    追加 トップページレイアウト変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import { useRouter } from 'next/router';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function HomePage() {
   const router = useRouter();
@@ -15,16 +16,14 @@ export default function HomePage() {
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-      height: 'calc(100% - 64px)', // Adjust this based on your AppBar height
+
     }}>
       <Box display="flex" alignItems="center" justifyContent="center" style={{ 
-        width: '100%',  // Set the container to occupy the full width
-        height: 'calc(100% - 64px)',  // Set a fixed height minus the AppBar height
-        overflow: 'hidden',  // This will ensure the video is cut off and doesn't overflow the container
+        width: '100%',
       }}>
         <img src="/logo.png" alt="Logo" style={{
-          maxHeight: '100%',  
-          maxWidth: '100%', 
+          maxHeight: '70%',  
+          maxWidth: '70%', 
           objectFit: 'contain'
         }}/>
       </Box>
@@ -35,13 +34,23 @@ export default function HomePage() {
           color: '#000000', 
           position: 'static', 
           marginTop: '20px', // Creates space between video and button
-          fontSize: '1.5em', // Adjust font size as needed
-          padding: '50px 80px', // Adjust padding as needed
+          fontSize: '1.0em', // Adjust font size as needed
+          padding: '10px 10px', // Adjust padding as needed
         }}
         onClick={handleClick}
       >
         はじめる
       </Button>
+      <Box width="50%" display="flex" justifyContent="center">
+        <Typography variant="h6" style={{ margin: '10px 0', textAlign: 'center' }}>
+          あなただけのプロフィール帳をTwitterでシェアしませんか？
+        </Typography>
+      </Box>
+      <img src="/template1.png" alt="Template1" style={{ 
+        maxHeight: '40%',  
+        maxWidth: '40%', 
+        objectFit: 'contain' 
+      }}/>
     </div>
   );
 }
