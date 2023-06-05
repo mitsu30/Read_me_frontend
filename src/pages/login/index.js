e81cdbd520ac2c1a852073ac724fb1c4be81b2b4
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-05 17:55:29 +0900

    追加 仮ログインページ

---
+++ src/pages/login/index.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+import useFirebaseAuth from "hooks/useFirebaseAuth";
+
+export default function LoginPage() {
+  const { loginWithGoogle } = useFirebaseAuth();
+
+  const handleGoogleLogin = () => {
+    const verifyIdToken = async () => {
+      const user = await loginWithGoogle();
+      const token = await user?.getIdToken();
+
+      const config = {
+        headers: { authorization: `Bearer ${token}` },
+      };
+
+      try {
+        axios.post("/auth", null, config);
+      } catch (err) {
+        let message;
+        if (axios.isAxiosError(err) && err.response) {
+          console.error(err.response.data.message);
+        } else {
+          message = String(err);
+          console.error(message);
+        }
+      }
+    };
+    verifyIdToken();
+  };
+
+  return (
+    <div>
+      <button onClick={handleGoogleLogin}>
+        <span>Sign in with Google</span>
+      </button>
+    </div>
+  );
+}
