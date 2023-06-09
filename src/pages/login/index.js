d90f53b825ddfb43fb22d25ce1c05e413ff8ab82
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-09 17:26:02 +0900

    追加 ログイン成功フラッシュ追加

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,13 @@
 import { useRouter } from 'next/router';
+import { useSnackbar } from 'notistack';
 import axios from "axios";
 import useFirebaseAuth from "../../hooks/useFirebaseAuth"
 
 export default function LoginPage() {
   const { loginWithFirebase } = useFirebaseAuth();
   const router = useRouter();
-
+  
+  const { enqueueSnackbar } = useSnackbar(); 
 
   const handleGitHubLogin = () => {
     const verifyIdToken = async () => {
@@ -24,6 +26,8 @@ export default function LoginPage() {
       try {
         console.log(details);
         axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth`, userDetails, config);
+        enqueueSnackbar('ログインに成功しました', { variant: 'success' });
+        router.push("/");
         // router.push('/additional_info_google');
       } catch (err) {
         let message;
