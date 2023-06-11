82fd3cb6a4e8a440daf5482148b35eed237f47e2
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-11 22:10:52 +0900

    追加 新規ユーザー追加情報入力画面レイアウト大枠完成

--- src/pages/additional_info/[id].js
+++ src/pages/additional_info/[id].js
@@ -4,7 +4,10 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
 
 export default function AdditionalInfoPage({ initialData }) {
   const router = useRouter();
@@ -12,15 +15,19 @@ export default function AdditionalInfoPage({ initialData }) {
   const { id } = router.query;
 
   const [username, setUsername] = useState(initialData.name);
-  const [avatar, setAvatar] = useState(""); 
-  // const [group, setGroup] = useState("");  // Add a new state for 'group'
-  
+  const [avatar, setAvatar] = useState(null);
+  const [preview, setPreview] = useState(null);
+
   const handleUpdateProfile = async () => {
     try {
-      const response = await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/${id}`, {
-        name: username,
-        avatar,
-        // group,
+      const formData = new FormData();
+      formData.append('name', username);
+      formData.append('avatar', avatar);
+
+      const response = await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/${id}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       });
 
       if (response.status === 200) {
@@ -35,36 +42,84 @@ export default function AdditionalInfoPage({ initialData }) {
     }
   };
 
+  const handleAvatarChange = (e) => {
+    setAvatar(e.target.files[0]);
+    setPreview(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
-    <div>
-      <Box component="form">
-        <Typography variant="h6">ユーザー情報の更新</Typography>
-
-        <label>ユーザーネーム:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-
-        <label>アバター画像:</label>
-        <input
-          type="file"
-          onChange={(e) => setAvatar(e.target.files[0])}
-        />
-
-        {/* <label>グループ:</label>  // Add a new input field for 'group'
-        <input
-          type="text"
-          value={group}
-          onChange={(e) => setGroup(e.target.value)}
-        /> */}
-
-        <Button variant="contained" onClick={handleUpdateProfile}>
-          更新
-        </Button>
-      </Box>
-    </div>
+    <Grid container component="main" justifyContent="center">
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            my: 2,
+            mx: 4,
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Typography component="h1" variant="h5">
+            ニックネームをおしえてね
+            </Typography>
+          </Box>
+          <Box component="form">
+            <TextField
+              color="secondary"
+              margin="normal"
+              fullWidth
+              id="username"
+              label="ニックネーム"
+              name="username"
+              autoFocus
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Box>
+          <Box>
+            <Typography component="h1" variant="h5">
+              アイコン用の画像をえらんでね
+            </Typography>
+          </Box>
+          <Box>
+            <TextField
+              color="secondary"
+              margin="normal"
+              fullWidth
+              type="file"
+              id="avatar"
+              name="avatar"
+              onChange={handleAvatarChange}
+              />
+          </Box>
+          {preview && (
+            <Box sx={{ display: 'flex', justifyContent: 'center', my: 2 }}>
+              <Avatar alt="アバター画像プレビュー" src={preview} />
+            </Box>
+          )}
+          <Box sx={{ display: 'flex', justifyContent: 'center', my: 2 }}>
+            <Button 
+              type="submit"
+              variant="contained"
+              sx={{ 
+                mt: 3, 
+                mb: 2, 
+                width: '30%', 
+                backgroundColor: '#FF6699',
+                '&:hover': {
+                  backgroundColor: '#E60073',
+                },
+                color: '#white',
+                fontWeight: 'bold'  
+              }}
+              onClick={handleUpdateProfile}
+            >
+              登録する
+            </Button>
+          </Box>
+        </Box> 
+      </Grid>
+    </Grid>
   );
 }
 
@@ -73,7 +128,6 @@ export async function getServerSideProps(context) {
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/${id}`);
   const data = await response.json();
-  console.log(data)
 
   return {
     props: {
