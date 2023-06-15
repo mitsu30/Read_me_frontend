0f627f08d9d275055f178c63878bf981ef70e5ed
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-15 12:36:30 +0900

    追加 ユーザー一覧画面所属グループ内のユーザーのみを表示

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -4,38 +4,56 @@ import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [groups, setGroups] = useState([]); 
   const [page, setPage] = useState(1);
   const [sortBy, setSortBy] = useState('created_at');
   const [order, setOrder] = useState('asc');
+  const [group, setGroup] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users?page=${page}&sort_by=${sortBy}&order=${order}`);
+      const result = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users?page=${page}&sort_by=${sortBy}&order=${order}&group_id=${group}`);
       setUsers(result.data);
       console.log(result.data)
     };
     fetchData();
-  }, [page, sortBy, order]);
+  }, [page, sortBy, order, group]);
 
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/groups/for_community/1`);
+      setGroups(response.data.groups); 
+      // const data = response.data.groups
+    };
+    fetchGroups();
+  }, []);
+    
   return (
     <div>
       <div>
         <Select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <MenuItem value="created_at">Created At</MenuItem>
-          <MenuItem value="name">Name</MenuItem>
+          <MenuItem value="name">ニックネーム</MenuItem>
         </Select>
         <Select value={order} onChange={(e) => setOrder(e.target.value)}>
-          <MenuItem value="asc">Ascending</MenuItem>
-          <MenuItem value="desc">Descending</MenuItem>
+          <MenuItem value="asc">昇順</MenuItem>
+          <MenuItem value="desc">降順</MenuItem>
+        </Select>
+        <Select value={group} onChange={(e) => setGroup(e.target.value)}>
+          <MenuItem value=''>RUNTEQのみんな</MenuItem>  
+          {groups.map((group) => ( // 取得したグループデータをもとに選択肢を動的に生成
+            <MenuItem key={group.id} value={group.id}>{group.name}</MenuItem>
+          ))}
         </Select>
       </div>
+
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Avatar</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Group</TableCell>
+              <TableCell></TableCell>
+              <TableCell>ニックネーム</TableCell>
+              <TableCell>所属</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
