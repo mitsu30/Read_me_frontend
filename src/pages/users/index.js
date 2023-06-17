9b919a30ca898b315b61b5599992fa22661e0a0a
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-17 18:33:08 +0900

    修正 環境変数改

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Select, MenuItem, IconButton, Grid } from '@mui/material';
 // import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { Avatar } from '@mui/material'; 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
