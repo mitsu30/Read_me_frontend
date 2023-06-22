4a42d99caee59b9587c5c11c1a53e963ea020255
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-22 16:07:04 +0900

    追加 プロフィール帳一覧画面大枠3

--- src/pages/profiles/index.js
+++ src/pages/profiles/index.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Card, CardMedia, Dialog, DialogContent, DialogActions, Button, Box, Grid  } from '@mui/material';
+import { Card, CardMedia, Dialog, DialogContent, DialogActions, Button, Box, Grid, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import nookies from "nookies";
 import axios from 'axios';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const StyledCard = styled(Card)(({ theme }) => ({
   width: 300, // カードの幅を固定
@@ -22,6 +24,8 @@ const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
 const DesignTemplates = ({ templates }) => {
   const [open, setOpen] = useState(false);
   const [currentTemplate, setCurrentTemplate] = useState(null);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleClickOpen = (template) => {
     setCurrentTemplate(template);
@@ -37,28 +41,43 @@ const DesignTemplates = ({ templates }) => {
   };
 
   return (
-    <Grid container spacing={2}>
+    <>
+    <Box sx={{ display: 'flex', justifyContent: 'center', my: 5 }}>
+    <Typography 
+      component="h1" 
+      variant="h1" 
+      fontWeight="bold" 
+      style={{ 
+        fontSize: isSmallScreen ? '2.5rem' : '4.5rem',  // 画面幅に応じてフォントサイズを変更
+      }}
+    >
+      デザインをえらぼう
+    </Typography>
+  </Box>
+
+    <Grid container spacing={2} justify="center"> 
       {templates.map((template) => (
         <Grid item key={template.id} onClick={() => handleClickOpen(template)}>
           <StyledCard>
             {template.name}
             <StyledCardMedia
-              component="img" // img要素を使用
+              component="img" 
               image={template.image_path}
             />
           </StyledCard>
         </Grid>
       ))}
   
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={open} onClose={handleClose} maxWidth="md"> 
         <DialogContent>
-          {currentTemplate && <img src={currentTemplate.image_path} alt="template" />}
+          {currentTemplate && <img src={currentTemplate.image_path} alt="template" style={{objectFit: 'contain', maxHeight: isSmallScreen ? '15vh' : '35vh'}} />}  
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleButtonClick}>次へ</Button>
+          <Button onClick={handleButtonClick} style={{ fontSize: '1.5rem', alignSelf: 'center' }}>これにする</Button>
         </DialogActions>
       </Dialog>
     </Grid>
+  </>
   );
 };
 
@@ -72,6 +91,5 @@ export async function getServerSideProps(context) {
   console.log(res.data)
   return { props: { templates: res.data} };
 }
-
 
 export default DesignTemplates;
