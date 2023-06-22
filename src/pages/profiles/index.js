748942e480abae6887c39f42d386316829bc70b9
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-22 13:09:01 +0900

    修正 mypage/editページでgroupの情報取得ロジック

--- src/pages/profiles/index.js
+++ src/pages/profiles/index.js
@@ -51,8 +51,8 @@ export async function getServerSideProps(context) {
   };
 
   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/templates`, config);
-  console.log(res)
-  return { props: { template: res.data.data } };
+  console.log(res.data)
+  return { props: { templates: res.data} };
 }
 
 
