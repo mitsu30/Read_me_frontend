a4da9a4ebf40913811505b9d9c73809ee6278595
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-22 14:35:50 +0900

    追加 プロフィール帳一覧画面大枠

--- src/pages/profiles/index.js
+++ src/pages/profiles/index.js
@@ -1,59 +1,73 @@
 import { useState } from 'react';
-import { Card, CardMedia, Dialog, DialogContent, DialogActions, Button } from '@mui/material'
+import { Card, CardMedia, Dialog, DialogContent, DialogActions, Button, Box, Grid  } from '@mui/material';
+import { styled } from '@mui/system';
 import nookies from "nookies";
 import axios from 'axios';
 
+const StyledCard = styled(Card)(({ theme }) => ({
+  maxWidth: 450, // カードの最大幅を設定
+  margin: theme.spacing(10) // カードの周りの余白を設定
+}));
+
+const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+  height: 0,
+  paddingTop: '52.36%', // 1.91:1
+  margin: theme.spacing(2),// 画像とカードの間の余白を設定
+}));
+
+
 const DesignTemplates = ({ templates }) => {
-    const [open, setOpen] = useState(false);
-    const [currentTemplate, setCurrentTemplate] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [currentTemplate, setCurrentTemplate] = useState(null);
 
-    const handleClickOpen = (template) => {
-        setCurrentTemplate(template);
-        setOpen(true);
-    };
+  const handleClickOpen = (template) => {
+      setCurrentTemplate(template);
+      setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+      setOpen(false);
+  };
 
-    const handleButtonClick = () => {
-        window.location.href = currentTemplate.nextUrl;
-    };
+  const handleButtonClick = () => {
+      window.location.href = currentTemplate.next_path;
+  };
 
-    return (
-        <div>
-            {templates.map((template) => (
-                <Card key={template.id} onClick={() => handleClickOpen(template)}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={template.imageUrl}
-                    />
-                </Card>
-            ))}
-
-            <Dialog open={open} onClose={handleClose}>
-                <DialogContent>
-                    {currentTemplate && <img src={currentTemplate.imageUrl} alt="template" />}
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleButtonClick}>次へ</Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
+  return (
+    <Grid container spacing={2}>
+      {templates.map((template) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={template.id} onClick={() => handleClickOpen(template)}>
+          <StyledCard>
+            {template.name}
+            <StyledCardMedia
+              image={template.image_path}
+            />
+          </StyledCard>
+        </Grid>
+      ))}
+  
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          {currentTemplate && <img src={currentTemplate.image_path} alt="template" />}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleButtonClick}>次へ</Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
 };
 
 export async function getServerSideProps(context) {
   const cookies = nookies.get(context);
   const config = {
-    headers: { authorization: `Bearer ${cookies.token}` },
+  headers: { authorization: `Bearer ${cookies.token}` },
   };
 
   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/templates`, config);
   console.log(res.data)
   return { props: { templates: res.data} };
-}
+  }
 
 
 export default DesignTemplates;
