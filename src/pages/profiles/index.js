3475f41b825e25194df0d012ab6d45a32255fdd4
西野充洋 <tnhtx.17.m@gmail.com>
2023-07-01 18:03:50 +0900

    修正 profiles_templateモーダル選び直しのボタン追加

--- src/pages/profiles/index.js
+++ src/pages/profiles/index.js
@@ -7,24 +7,24 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 
   const StyledCard = styled(Card)(({ theme }) => ({
-    width: 280, // カードの幅を固定
-    margin: theme.spacing(1), // カードの周りの余白を設定
-    padding: theme.spacing(1), // カードの内側の余白を設定
-    display: 'flex', // カードの内容をフレックスボックスとして扱う
-    flexDirection: 'column', // カードの方向をカラム（垂直）に設定
-    alignItems: 'center', // カードの内容を中央に配置
-    transition: 'transform 0.3s', // transform プロパティの変化を0.3秒間で遷移させる
-    boxShadow: '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)', // カードに影をつける
-    backgroundColor: '#f9f9f9', // カードの背景色を設定
-    border: '1px solid #ddd', // カードの境界線を設定
+    width: 280, 
+    margin: theme.spacing(1), 
+    padding: theme.spacing(1), 
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: 'center',
+    transition: 'transform 0.3s', 
+    boxShadow: '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)', 
+    backgroundColor: '#f9f9f9', 
+    border: '1px solid #ddd', 
     '&:hover': {
-      transform: 'scale(1.05) rotate(3deg)', // マウスオーバー時にカードを1.05倍に拡大し、3度回転させる
+      transform: 'scale(1.05) rotate(3deg)', 
     },
   }));
 
   const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
-    width: 250, // 画像の幅を200pxに固定
-    objectFit: 'contain', // 画像を全体が見えるように配置
+    width: 250, 
+    objectFit: 'contain', 
   }));
 
 const DesignTemplates = ({ templates }) => {
@@ -75,18 +75,20 @@ const DesignTemplates = ({ templates }) => {
         </Box>
       </Grid>
     ))}
-      <Dialog open={open} onClose={handleClose} maxWidth="md"> 
-        <DialogContent>
-          {currentTemplate && <img src={currentTemplate.image_path} alt="template" style={{objectFit: 'contain', maxHeight: isSmallScreen ? '15vh' : '35vh'}} />}  
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleButtonClick} style={{ fontSize: '1.5rem', alignSelf: 'center' }}>これにする</Button>
-        </DialogActions>
-      </Dialog>
-    </Grid>
-  </>
-  );
-};
+
+    <Dialog open={open} onClose={handleClose} maxWidth="md"> 
+          <DialogContent>
+            {currentTemplate && <img src={currentTemplate.image_path} alt="template" style={{objectFit: 'contain', maxHeight: isSmallScreen ? '15vh' : '35vh'}} />}  
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleButtonClick} style={{ fontSize: '1.0rem', alignSelf: 'center' }}>これにする</Button>
+            <Button onClick={handleClose} style={{ fontSize: '1.0rem', alignSelf: 'center' }}>えらびなおす</Button> 
+          </DialogActions>
+        </Dialog>
+      </Grid>
+    </>
+    );
+  };  
 
 export async function getServerSideProps(context) {
   const cookies = nookies.get(context);
