d42eea2f6bd10597832943d915e597e471f27f50
西野充洋 <tnhtx.17.m@gmail.com>
2023-07-17 15:14:25 +0900

    fix_show_publicへのアクセスをコメントアウト

--- src/pages/users/profiles/[id].js
+++ src/pages/users/profiles/[id].js
@@ -55,8 +55,8 @@ export default function ProfilePage() {
       }
 
       const res = cookies.token 
-        ? await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/profiles/base/show_for_community/${id}`, config) 
-        : await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/profiles/base/show_public/${id}`);
+        await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/profiles/base/show_for_community/${id}`, config) 
+        // : await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/profiles/base/show_public/${id}`);
 
       setProfileImage(res.data);
       setLoading(false);
