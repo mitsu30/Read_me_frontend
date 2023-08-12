c12ba7a487ce20b97c36daad3f474635cbbf0683
西野充洋 <tnhtx.17.m@gmail.com>
2023-08-12 21:37:37 +0900

    update_みにまむとべーしっくXシェア時#タグからスクール名消去

--- src/pages/profiles/[id].js
+++ src/pages/profiles/[id].js
@@ -56,10 +56,10 @@ export default function ResultPage({ profileImage }) {
     let tweetText;
     switch(profileImage.template_id) {
       case 1:
-        tweetText = 'みんなよろしく♪ #りーどみー #RUNTEQ #大人のプロフィール帳';
+        tweetText = 'みんなよろしく♪ #りーどみー #大人のプロフィール帳';
         break;
       case 2:
-        tweetText = 'わたしのプロフィール帳みんなみてね♪ #りーどみー #RUNTEQ #大人のプロフィール帳';
+        tweetText = 'わたしのプロフィール帳みんなみてね♪ #りーどみー #大人のプロフィール帳';
         break;
       case 3:
         tweetText = 'ひよっこエンジニアなかま募集中♪ #駆け出しエンジニアと繋がりたい #りーどみー #RUNTEQ';
@@ -68,7 +68,7 @@ export default function ResultPage({ profileImage }) {
         tweetText = '4周年おめでとう!! #RUNTEQ #りーどみー';
         break;
       default:
-        tweetText = 'わたしのプロフィール！みんなよろしく♪ #りーどみー #RUNTEQ #大人のプロフィール帳';
+        tweetText = 'わたしのプロフィール！みんなよろしく♪ #りーどみー #大人のプロフィール帳';
         break;
     }
     setTimeout(() => {
